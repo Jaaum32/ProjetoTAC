a76b6049e35c17f880c267f8dd1abf19c21512b2
elrick046 <87386997+elrick046@users.noreply.github.com>
2024-12-18 13:22:25 -0300

    TIVE QUE TIRAR OS COMENTARIOS

--- API2Node/middlewares/isAuthenticated.js
+++ API2Node/middlewares/isAuthenticated.js
@@ -1,20 +1,18 @@
  const jwt = require("jsonwebtoken");
 
-// const isAuthenticated = (req,res,next) => {
-//     const { authorization } = req.headers;
+ const isAuthenticated = (req,res,next) => {
+     const { authorization } = req.headers;
 
-//     if(!authorization) {
-//         return res.status(401).json({message: "Sem token"});
-//     }
-//     jwt.verify(authorization, process.env.SECRET, (err, decoded) => {
-//         if(err) {
-//             return res.status(401).json({message: "Token inválido"});
-//         }
-//         req.userId = decoded.id;
-//         return next();
-//     });
-// }
-
-const isAuthenticated = (req, red, next) => {return next()};
-
+     if(!authorization) {
+         return res.status(401).json({message: "Sem token"});
+     }
+     jwt.verify(authorization, process.env.SECRET, (err, decoded) => {
+         if(err) {
+             return res.status(401).json({message: "Token inválido"});
+         }
+         req.userId = decoded.id;
+         return next();
+     });
+ }
+ 
  module.exports = isAuthenticated;
